8dad01063497b9ee1ceaca7ea21b3cdf60f7dd53
Miłosz Szczepanek <milosz23wow@gmail.com>
2020-03-10 11:56:16 +0100

    rn init

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  preset: "react-native",
+  transformIgnorePatterns: ["node_modules/(?!(jest-)?react-native|react-navigation|@react-navigation/.*)"],
+  collectCoverageFrom: ["app/**/*.{js,ts,tsx}", "!**/node_modules/**", "!**/vendor/**"]
+};
